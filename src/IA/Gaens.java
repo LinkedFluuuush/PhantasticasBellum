b1201cd675fbb815cce7053881c0691c38b9fbfb
LinkedFluuuush <linkedfluuuush@gmail.com>
2015-04-01 17:43:14 +0200

    Mise en place de l'algo alphaBeta

--- src/IA/Gaens.java
+++ src/IA/Gaens.java
@@ -23,49 +23,48 @@ public class Gaens extends AbstractIA{
     private Coup alphaBeta(int alpha, int beta, Partie p){
         int alphaTmp;
         for(Coup c : p.getTousCoups()){
-            alphaTmp = alphaBetaMin(c, p, 1, 0, 0, alpha, beta);
+            alphaTmp = alphaBetaVal(c, p, 1, 0, 0, alpha, beta);
             if(alphaTmp > alpha){
                 alpha = alphaTmp;
                 memoriseCoup(c);
             }
         }
 
-
         return getCoupMemorise();
     }
 
-    private int alphaBetaMin(Coup c, Partie p, int profMax, int profActuelle, int coutCumuleActuel, int alpha, int beta){
-        int vMin = 0, vFils;
+    private int alphaBetaVal(Coup c, Partie p, int profMax, int profActuelle, int coutCumuleActuel, int alpha, int beta) {
+        int alphaTemp, betaTemp, val;
         int cout = getHeuristique(c) + coutCumuleActuel;
         Partie pClone = p.clone();
         pClone.appliquerCoup(c);
 
-        if(pClone.estTerminee()){
+        if (pClone.estTerminee() || profActuelle == profMax) {
             return cout;
         } else {
-            for(Coup cTmp : p.getTousCoups()){
-                vFils = alphaBetaMax(cTmp, p, profMax, profActuelle++, cout, alpha, beta);
-                vMin = Math.min(vFils, vMin);
+            if(p.getJoueurActuel() instanceof Gaens) { //Noeud Max
+                alphaTemp = -9999;
+                for(Coup nCoup : p.getTousCoups()){
+                    val = alphaBetaVal(nCoup, pClone, profMax, profActuelle++, cout, Math.max(alpha, alphaTemp), beta);
+                    alphaTemp = Math.max(alphaTemp, val);
+
+                    if(alphaTemp >= beta){
+                        return alphaTemp;
+                    }
+                }
+                return alphaTemp;
+            } else { //Noeud Min
+                betaTemp = 9999;
+                for (Coup nCoup : p.getTousCoups()) {
+                    val = alphaBetaVal(nCoup, pClone, profMax, profActuelle++, cout, alpha, Math.min(beta, betaTemp));
+                    betaTemp = Math.min(betaTemp, val);
+
+                    if(betaTemp <= alpha){
+                        return betaTemp;
+                    }
+                }
+                return betaTemp;
             }
         }
-
-        return vMin;
-    }
-
-    private int alphaBetaMax(Coup c, Partie p, int profMax, int profActuelle, int coutCumuleActuel, int alpha, int beta){
-        int vMax = 0, vFils;
-        int cout = getHeuristique(c) + coutCumuleActuel;
-        Partie pClone = p.clone();
-        pClone.appliquerCoup(c);
-
-        if(pClone.estTerminee()){
-            return cout;
-        } else {
-            for(Coup cTmp : p.getTousCoups()){
-                vFils = alphaBetaMin(cTmp, p, profMax, profActuelle++, cout, alpha, beta);
-                vMax = Math.max(vFils, vMax);
-            }
-        }
-        return vMax;
     }
 }
